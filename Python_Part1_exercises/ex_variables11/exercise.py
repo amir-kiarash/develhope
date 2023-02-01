2d19fe675aebb6b81057024f0dd456e63fe616a0
amir <km.kiarash@yahoo.com>
2023-02-01 15:50:18 +0200

    Exercise Variables 11

---
+++ Python_Part1_exercises/ex_variables11/exercise.py
@@ -0,0 +1,6 @@
+a = 'hello'.capitalize() #capitalize 
+b = 'tom'.upper() #uppercase
+c = 'LAURA'.lower() #lowercase
+question = 'How are you'.replace('o', 'e') #change o in e
+age_question = '{} {} {} {}?'.format('How', 'old', 'are', 'you') #use the correct method to create a string for each word
+print(a, b, c, question, age_question)
