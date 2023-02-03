ebf01f3aeb6e11f690700cad89f2540cd8e48ece
amir <km.kiarash@yahoo.com>
2023-02-03 09:41:15 +0200

    Exercise Methods 2

---
+++ Python_Part1_exercises/ex_methods2/exercise.py
@@ -0,0 +1,9 @@
+num1 = 1.3
+num2 = 2.3
+num3 = 1j 
+num4 = 1.4 
+num5 = 1.5
+
+print(f"{float(num1)}, {int(num2)}, {complex(num3)}, {round(num4)}, {round(num5)} ")
+
+print(type(float(num1)), type(int(num2)), type(complex(num3)), type(round(num4)), type(round(num5)))
